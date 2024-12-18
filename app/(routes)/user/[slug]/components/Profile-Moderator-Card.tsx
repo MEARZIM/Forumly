ca746d7c20cf0ed6bbb0fcd26a25e9c6d57ff30a
AnkitaPal-2002 <ankitapal6112002@gmail.com>
2024-12-18 22:00:33 +0530

    fix: layout issues and improve UI on profile page

--- app/(routes)/user/[slug]/components/Profile-Moderator-Card.tsx
+++ app/(routes)/user/[slug]/components/Profile-Moderator-Card.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { getAuthSession } from '@/auth';
 import { db } from '@/lib/db';
+import CreateCommunity from '@/components/layouts/CreateCommunity';
 
 interface Props {
     username: string;
@@ -39,7 +40,7 @@ const ProfileModeratorCard = async ({ username }: Props) => {
                             {subforumsWithMemberCount.map((subforum) => (
                                 <div key={subforum.id} className="flex items-center justify-between">
                                     <div className="text-sm">
-                                        <p className="font-medium text-base">r/{subforum.name}</p>
+                                        <p className="font-medium text-base">f/{subforum.name}</p>
                                         <p className="text-xs text-muted-foreground">{subforum.memberCount} members</p>
                                     </div>
                                 </div>
@@ -47,26 +48,9 @@ const ProfileModeratorCard = async ({ username }: Props) => {
                         </div>
                     </CardContent>
                 </Card>
-            ) : (
-                <Card>
-                    <CardContent className="p-4">
-                        <p className="font-semibold text-base mb-4">MODERATOR OF THESE COMMUNITIES</p>
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center gap-2">
-                                <Avatar className="w-8 h-8">
-                                    <AvatarImage src="/placeholder.svg" />
-                                    <AvatarFallback className="text-sm">R</AvatarFallback>
-                                </Avatar>
-                                <div className="text-sm">
-                                    <p className="font-medium">r/RanbirKapoor</p>
-                                    <p className="text-xs text-muted-foreground">48K members</p>
-                                </div>
-                                <Button size={"sm"}>Join</Button>
-                            </div>
-                        </div>
-                    </CardContent>
-                </Card>
-            )}
+            ) : <>
+                <CreateCommunity />
+            </>}
         </>
     );
 };
